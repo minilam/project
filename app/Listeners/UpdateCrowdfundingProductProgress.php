d994feec886c36303fd4c62cc799386e534f401b
hongliang <liuforworking@163.com>
2019-02-15 10:35:52 +0800

    feat:  queue table

--- app/Listeners/UpdateCrowdfundingProductProgress.php
+++ app/Listeners/UpdateCrowdfundingProductProgress.php
@@ -22,22 +22,22 @@ class UpdateCrowdfundingProductProgress implements ShouldQueue
         if ($order->type !== Order::TYPE_CROWDFUNDING) {
             return;
         }
-        $crowdfunding = $order->itmes[0]->product->crowdfunding;
+        $crowdfunding = $order->items[0]->product->crowdfunding;
 
         $data = Order::query()->where('type', Order::TYPE_CROWDFUNDING)
             ->whereNotNull('paid_at')
-            ->whereHas('items', function($query){
+            ->whereHas('items', function($query) use ($crowdfunding) {
                 $query->where('product_id', $crowdfunding->product_id);
             })->first([
                 //  取出订单总额
                 \DB::raw('sum(total_amount) as total_amount'),
                 // 去除去重的支持团友数
-                \DB::raw('count(distinct(user_id) as user_count'),
+                \DB::raw('count(distinct(user_id)) as user_count'),
             ]);
         
         $crowdfunding->update([
             'total_amount' => $data->total_amount,
-            'user_count' => $user_count
+            'user_count' => $data->user_count
         ]);
     }
 }
--- config/pay.php
+++ config/pay.php
@@ -1,22 +1,23 @@
 <?php
 
 return [
+    // 支付宝支付配置
     'alipay' => [
-        'app_id'         => '',
-        'ali_public_key' => '',
-        'private_key'    => '',
-        'log'            => [
+        'app_id' => env('ALIPAY_APP_ID', ''),
+        'ali_public_key' => env('ALIPAY_PUBLIC_KEY', ''),
+        'private_key' => env('ALIPAY_PRIVATE_KEY', ''),
+        'log' => [
             'file' => storage_path('logs/alipay.log'),
         ],
     ],
-
+    // 微信支付配置
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
-        'log'         => [
+        'app_id' => env('WECHAT_APP_ID', ''), // 公众号: app id
+        'mch_id' => env('MERCHANT_ID', ''), // 第一步获取到的商户号
+        'key' => env('WECHAT_PRIVATE_KEY', ''), // 刚刚设置的 API 密钥
+        'cert_client' => '', // 证书路径
+        'cert_key' => '',
+        'log' => [
             'file' => storage_path('logs/wechat_pay.log'),
         ],
     ],
