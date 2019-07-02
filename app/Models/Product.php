a9320aa3f238f92497046fb0a8c3853946692afd
hongliang <liuforworking@163.com>
2019-07-02 19:51:17 +0800

    feat: 部署

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -54,7 +54,7 @@ class Product extends Model
 
     public function seckill()
     {
-        return $this->hasOne(seckillProduct::class);
+        return $this->hasOne(SeckillProduct::class);
     }
 
     // 属性
